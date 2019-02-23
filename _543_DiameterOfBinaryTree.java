a0ab7551f9fc2d9891187e2a9e285acd8f7862c3
rayhan <hossain.rayhan@outlook.com>
2019-02-23 17:02:29 -0500

    LeetCode-543: Diameter of a Binary Tree, Max Depth

---
+++ _543_DiameterOfBinaryTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+//https://leetcode.com/problems/diameter-of-binary-tree/discuss/101132/Java-Solution-MaxDepth
+class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+    
+    public int maxDepth(TreeNode root){
+        if(root == null)
+            return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.max(max, left+right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
