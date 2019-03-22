7dba39e54b1db0477ace69457487d590ac122f5e
rayhan <hossain.rayhan@outlook.com>
2019-03-21 21:00:23 -0400

    LeetCode- 226: Invert Binary Tree

---
+++ _226_InvertBinaryTree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        
+        TreeNode l = invertTree(root.left);
+        TreeNode r = invertTree(root.right);
+        root.left = r;
+        root.right = l;
+        
+        return root;
+    }
+}
