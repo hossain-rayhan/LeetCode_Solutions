e0e3e91dfad235f39ef7ea9f71e49caeef5d0ba1
rayhan <hossain.rayhan@outlook.com>
2019-03-24 20:23:59 -0400

    LeetCode- 056: Merge intervals

--- _056_MergeIntervals.java
+++ _056_MergeIntervals.java
@@ -9,18 +9,44 @@
  */
 class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        
+        //return merge1(intervals);
+        return merge2(intervals);
+    }
+    
+    //sort start and end separately
+    public List<Interval> merge2(List<Interval> intervals){
+        int n = intervals.size();
+	    int[] starts = new int[n];
+	    int[] ends = new int[n];
+	    for (int i = 0; i < n; i++) {
+		    starts[i] = intervals.get(i).start;
+            ends[i] = intervals.get(i).end;
+	    }
+	    Arrays.sort(starts);
+	    Arrays.sort(ends);
+	    // loop through
+	    List<Interval> res = new ArrayList<Interval>();
+	    for (int i = 0, j = 0; i < n; i++) { // j is start of interval.
+		    if (i == n - 1 || starts[i + 1] > ends[i]) {
+			    res.add(new Interval(starts[j], ends[i]));
+			    j = i + 1;
+		    }
+	    }
+	    return res;
+    }
+    
+    public List<Interval> merge1(List<Interval> intervals){
         Collections.sort(intervals, new Comparator<Interval>(){
             public int compare(Interval i1, Interval i2){
                 return i1.start - i2.start;
             }
         });
-        
+
         for(Interval i : intervals){
             System.out.println(i.start + " " + i.end);
         }
-        List<Interval> result = new ArrayList<>();
         
+        List<Interval> result = new ArrayList<>();
         for(Interval iv : intervals){
             if(result.isEmpty()){
                 result.add(iv);
@@ -32,8 +58,7 @@ class Solution {
                     result.add(iv);
                 }
             }
-        }
-        
+        }        
         return result;
     }
 }
