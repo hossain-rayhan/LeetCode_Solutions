6f4dee9af7a63aa397ef15c34abb25631804064f
rayhan <hossain.rayhan@outlook.com>
2019-03-26 00:32:49 -0400

    LeetCode- 020: Valid Parentheses

--- _020_ValidParentheses.java
+++ _020_ValidParentheses.java
@@ -1,5 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
+        //return normal(s);
+        return smart(s);
+    }
+    
+    public boolean smart(String s){
+        Stack<Character> stack = new Stack();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '{'){
+                stack.push('}');
+            }else if(c == '['){
+                stack.push(']');
+            }else{
+                if(stack.isEmpty() || stack.pop() != c){
+                    return false;
+                }
+            }
+        }
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean normal(String s){
         Stack<Character> stack = new Stack<>();
         
         for(int i = 0; i < s.length(); i++){
