feb0ec28d3b1ae6e1f3c01cd9e66129b562e0fec
rayhan <hossain.rayhan@outlook.com>
2019-03-30 22:45:13 -0400

    LeetCode- 973: K closest points to origin, 3 solutions

--- _973_KClosestPoints.java
+++ _973_KClosestPoints.java
@@ -2,6 +2,22 @@
 
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
+        //return sort(points, K);
+        //return maxHeap(points, K);
+        return quickSelect(points, K);
+    }
+    
+    //solution-1: sort and return first K
+    //Time: O(NlogN)
+    public int[][] sort(int[][] points, int K){
+        Arrays.sort(points, (p1, p2) -> (p1[0] * p1[0] + p1[1] * p1[1]) - (p2[0]*p2[0] + p2[1]*p2[1]));
+        
+        return Arrays.copyOfRange(points, 0, K);
+    }
+    
+    //solution-2: Maintain a heap of size K
+    //Time: O(NlogK)
+    public int[][] maxHeap(int[][] points, int K){
         PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((a,b) -> (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]));
         
         for(int[] p : points){
@@ -18,5 +34,37 @@ class Solution {
         }
         
         return result;
+    }
+    
+    //solution-3: Quick select
+    //Time: Theoritically O(N), but in the worst case it might be O(N^2).
+    public int[][] quickSelect(int[][] points, int K){
+        int left = 0, right = points.length - 1;
+        while(left <= right){
+            int index = partition(points, left, right);
+            if(index == K){
+                break;
+            }else if(index < K){
+                left = index + 1;
+            }else{
+                right = index - 1;
+            }
+        }
+        return Arrays.copyOfRange(points, 0, K);
+    }
+    public int partition(int[][] points, int left, int right){
+        int[] pivot = points[left];
+        
+        while(left < right){
+            while(left < right && compare(points[right], pivot) >= 0) right--;
+            points[left] = points[right];
+            while(left < right && compare(points[left], pivot) <= 0) left++;
+            points[right] = points[left];
+        }
+        points[left] = pivot;
+        return left;
+    }
+    public int compare(int[] a, int[] b){
+        return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]);
     }
 }
