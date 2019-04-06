d9e9d7a71f2376db2b7e195651a83343179d08b7
rayhan <hossain.rayhan@outlook.com>
2019-04-06 17:23:37 -0400

    LeetCode- 957: Prison Cells After N Days, Array Manipulation

--- _957_PrisonCellsAfterNDays.java
+++ _957_PrisonCellsAfterNDays.java
@@ -1,41 +1,21 @@
 class Solution {
     public int[] prisonAfterNDays(int[] cells, int N) {
-        N = N % 14; // The pattern is repeated after 14th
-        if(N == 0){
-            N = 14;
-        }
+        //the pattern is repeated somewhere in the middle
+        int[] f_cells = new int[cells.length];
+        int[] next_cells = new int[cells.length];
         
-        int[] cellNew = new int[cells.length];
-        cellNew[0] = 0;
-        cellNew[cells.length-1] = 0;
-        
-        for(int i = 1; i <= N; i++){
-            if(i %2 == 1){
-                for(int j = 1; j < cells.length - 1; j++){
-                    if(cells[j-1] == cells[j+1]){
-                        cellNew[j] = 1;
-                    }else{
-                        cellNew[j] = 0;
-                    }
-                }
-            }else{
-                for(int j = 1; j < cells.length - 1; j++){
-                    if(cellNew[j-1] == cellNew[j+1]){
-                        cells[j] = 1;
-                    }else{
-                        cells[j] = 0;
-                    }
-                }
-                cells[0] = 0;
-                cells[cells.length-1] = 0;
+        for(int cycle = 0; N--> 0; cycle++){
+            for(int i = 1; i < cells.length - 1; i++){
+                next_cells[i] = cells[i-1] == cells[i+1]? 1 : 0;
             }
             
+            if(cycle == 0){// very first iteration
+                f_cells = next_cells.clone();
+            }else if(Arrays.equals(next_cells, f_cells)){ //cycle found
+                N = N % cycle;
+            }
+            cells = next_cells.clone(); //every time update the cells
         }
-        
-        if(N%2 == 0){
-            return cells;
-        }else{
-            return cellNew;
-        }
+        return cells;
     }
 }
