ba8aefb8ca6df2f1de44385efd87935e4fee9fc8
rayhan <hossain.rayhan@outlook.com>
2019-04-05 23:33:44 -0400

    LeetCode- 002: Linked List, Add two Numbers

--- _002_AddTwoNumbers.java
+++ _002_AddTwoNumbers.java
@@ -8,55 +8,33 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, current = null;
         int carry = 0;
-        ListNode root = null, current = null;
-        while(l1 != null && l2 != null){
-            int sum = l1.val + l2.val + carry;
-            carry = sum / 10;
-            sum = sum % 10;
-            ListNode newNode = new ListNode(sum);
-            if(root == null){
-                    root = newNode;
+        while(l1 != null || l2 != null){
+            int val1 = 0, val2 = 0;
+            if(l1 != null){
+                val1 = l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+            int val = (val1 + val2 + carry) % 10;
+            carry = (val1 + val2 + carry) / 10;
+            ListNode newNode = new ListNode(val);
+            if(head == null){
+                    head = newNode;
                     current = newNode;
             }else{
                     current.next = newNode;
                     current = current.next;
             }
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while(l1 != null){
-            int sum = l1.val + carry;
-            carry = sum / 10;
-            sum = sum % 10;
-            ListNode newNode = new ListNode(sum);
-            if(root == null){
-                    root = newNode;
-                    current = newNode;
-            }else{
-                    current.next = newNode;
-                    current = current.next;
-            }
-            l1 = l1.next;
-        }
-        while(l2 != null){
-            int sum = l2.val + carry;
-            carry = sum / 10;
-            sum = sum % 10;
-            ListNode newNode = new ListNode(sum);
-            if(root == null){
-                    root = newNode;
-                    current = newNode;
-            }else{
-                    current.next = newNode;
-                    current = current.next;
-            }
-            l2 = l2.next;
         }
         if(carry > 0){
             ListNode newNode = new ListNode(carry);
             current.next = newNode;
         }
-        return root;
+        return head;
     }
 }
