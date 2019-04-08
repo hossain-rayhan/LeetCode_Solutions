1fbdeb17efaf55a442a8d279d4dba17b2729cd49
rayhan <hossain.rayhan@outlook.com>
2019-04-08 01:10:25 -0400

    LeetCode- 004: Median of two sorted array, log(m+n) time

--- _004_MedianOfTwoSortedArrays.java
+++ _004_MedianOfTwoSortedArrays.java
@@ -1,3 +1,44 @@
+//https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2496/Concise-JAVA-solution-based-on-Binary-Search
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int l = m + n;
+        
+        if(l % 2 == 0){
+            int left = findKth(nums1, 0, nums2, 0, l / 2);
+            int right = findKth(nums1, 0, nums2, 0, (l / 2) + 1);
+            return (double) (left + right)/ 2.0;
+        }else{
+            return (double) findKth(nums1, 0, nums2, 0, (l / 2) + 1);
+        }
+    }
+    
+    public int findKth(int[] a, int aStart, int[] b, int bStart, int k){
+        if(aStart >= a.length){
+            return b[bStart + k - 1];
+        }
+        if(bStart >= b.length){
+            return a[aStart + k - 1];
+        }
+        if(k == 1){
+            return Math.min(a[aStart] , b[bStart]);
+        }
+        
+        int aMid = aStart + k/2 - 1;
+        int bMid = bStart + k/2 - 1;
+        int aVal = aMid >= a.length? Integer.MAX_VALUE : a[aMid];
+        int bVal = bMid >= b.length? Integer.MAX_VALUE : b[bMid];
+        
+        if(aVal <= bVal){
+            return findKth(a, aMid + 1, b, bStart, k - k/2);
+        }else{
+            return findKth(a, aStart, b, bMid + 1, k - k/2);
+        }
+    }
+}
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int l1 = nums1.length;
