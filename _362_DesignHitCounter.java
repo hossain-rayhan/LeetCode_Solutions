ca7992dd88fa0544f3ebb40d989d880ece4febea
rayhan <hossain.rayhan@outlook.com>
2019-04-22 14:46:13 -0400

    LeetCode- 362: Design Hit Counter

---
+++ _362_DesignHitCounter.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/design-hit-counter/discuss/83483/Super-easy-design-O(1)-hit()-O(s)-getHits()-no-fancy-data-structure-is-needed!
+class HitCounter {
+
+    int[] times;
+    int[] hits;
+    
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        times = new int[300];
+        hits = new int[300];
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        int index = timestamp % 300;
+        if(times[index] != timestamp){
+            times[index] = timestamp;
+            hits[index] = 1;
+        }else{
+            hits[index]++;
+        }
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        int hitCounter = 0;
+        for(int i = 0; i < times.length; i++){
+            if(timestamp - times[i] < 300){
+                hitCounter += hits[i];
+            }
+        }
+        return hitCounter;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
